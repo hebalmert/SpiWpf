b3c30154a53816065f0cb3eaf5252a3d9a2af9ab
Hebert Merchan <merchanhebert@gmail.com>
2024-08-21 12:40:29 -0400

    MainPage con la ejecucion de los botones listos.  Llama a las nuevas Pages dentro del MainPage.

---
+++ SpiWpf.App/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SpiWpf.Wpf.ViewModels
+{
+    public class HomeViewModel : ObservableObject
+    {
+
+    }
+}
--- SpiWpf.App/ViewModels/MainViewModel.cs
+++ SpiWpf.App/ViewModels/MainViewModel.cs
@@ -1,13 +1,35 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SpiWpf.Wpf.Views;
 
 namespace SpiWpf.Wpf.ViewModels
 {
-    public class MainViewModel : ObservableObject
+    public partial class MainViewModel : ObservableObject
     {
+
+        private ObservableObject _CurrentChildView;
+
+        public ObservableObject? CurrentChildView
+        {
+            get => _CurrentChildView;
+            set { SetProperty(ref _CurrentChildView!, value); }
+        }
 
         public MainViewModel()
         {
             
+        }
+
+        [RelayCommand]
+        public async Task LoadHomeView()
+        {
+            CurrentChildView = new HomeViewModel();
+        }
+
+        [RelayCommand]
+        public async Task LoadClientsView()
+        {
+            CurrentChildView = new ClientsViewModel();
         }
     }
 }
