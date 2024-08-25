595e4b919279836a09056513886d3e158f7841a8
Hebert Merchan <merchanhebert@gmail.com>
2024-08-25 00:25:22 -0400

    Listo el sistema de Activacion de Clientes

--- SpiWpf.App/ViewModels/SuspendedViewModel.cs
+++ SpiWpf.App/ViewModels/SuspendedViewModel.cs
@@ -1,13 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using SpiWpf.Data;
 using SpiWpf.Entities.Models;
-using System.Collections.Generic;
+using SpiWpf.Wpf.Helpers;
 using System.Collections.ObjectModel;
+using System.Net.NetworkInformation;
 using System.Windows;
 
 namespace SpiWpf.Wpf.ViewModels
 {
-    public class SuspendedViewModel : ObservableObject
+    public partial class SuspendedViewModel : ObservableObject
     {
         public ObservableCollection<SuspendedAPI>? SuspendedAPILst { get; set; }
 
@@ -74,6 +76,76 @@ namespace SpiWpf.Wpf.ViewModels
                     }
                 }
             }
+        }
+
+        [RelayCommand]
+        public async Task SuspendedToActive(int IdSuspended)
+        {
+            IsLoading = true;
+
+            var responseHttp = await Repository.Get<SuspendedActiveAPI>($"/api/suspended/toactive/{IdSuspended}");
+            if (responseHttp.Error)
+            {
+                IsLoading = false;
+                var msgerror = await responseHttp.GetErrorMessageAsync();
+                MessageBox.Show($"{msgerror}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            SuspendedActiveAPI DatoEliminar = responseHttp.Response;
+
+            //Verificamos el Ping al servidor para ver si responde y continuar
+            Ping ping = new();
+            PingReply reply = await ping.SendPingAsync(DatoEliminar.IpNetwork!, 3000);
+            if (reply.Status != IPStatus.Success)
+            {
+                // El ping fue exitoso
+                IsLoading = false;
+                MessageBox.Show($"Fallo el ping a {DatoEliminar.IpNetwork}. Estado: {reply.Status}", "Respuesta Conexion", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            //Termina prueba del Ping
+
+
+            ////////////////////////////////////////////////////////////
+            MK mikrotik = new MK(DatoEliminar.IpNetwork!, DatoEliminar.ApiPort);
+            if (!mikrotik.Login(DatoEliminar.Usuario!, DatoEliminar.Clave!))
+            {
+                IsLoading = false;
+                MessageBox.Show($"Error en la Conexion al Servidor Mikrotik", "Error Conexion", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            mikrotik.Send("/ip/hotspot/ip-binding/set");
+            mikrotik.Send("=.id=" + DatoEliminar.IdIpBinding);
+            mikrotik.Send("=type=" + "bypassed");
+            mikrotik.Send("/ip/hotspot/ip-binding/print", true);
+
+            int total = 0;
+            int rest = 0;
+            string idmk;
+
+            foreach (var item2 in mikrotik.Read())
+            {
+                idmk = item2;
+                total = idmk.Length;
+                rest = total - 10;
+            }
+
+            mikrotik.Close();
+
+            var responseHttp2 = await Repository.Delete($"/api/suspended/toDelete/{IdSuspended}");
+            if (responseHttp.Error)
+            {
+                IsLoading = false;
+                var msgerror = await responseHttp.GetErrorMessageAsync();
+                MessageBox.Show($"{msgerror}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+           
+            await LoadSuspended();
+
+            IsLoading = false;
+            MessageBox.Show($"El Cliente se ha Activado con Exitro", "Confirmacion Exitosa", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
---
+++ SpiWpf.Entities/Models/SuspendedActiveAPI.cs
@@ -0,0 +1,17 @@
+﻿namespace SpiWpf.Entities.Models
+{
+    public class SuspendedActiveAPI
+    {
+        public string? ServerName { get; set; }
+
+        public string? IpNetwork { get; set; }
+
+        public string? Usuario { get; set; }
+
+        public string? Clave { get; set; }
+
+        public int ApiPort { get; set; }
+
+        public string? IdIpBinding { get; set; }
+    }
+}
